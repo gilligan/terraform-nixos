0fa14f38d0d234c00f6d1294fc4167a443ce5eff
Tobias Pflug <tobias.pflug@gmail.com>
2021-03-14 21:34:00 +0100

    update to nixos ami with web-ghc

--- aws_image_nixos/url_map.tf
+++ aws_image_nixos/url_map.tf
@@ -3,7 +3,7 @@ variable "url_map" {
   type = map(string)
 
   default = {
-    "latest.eu-west-1.hvm-ebs" = "ami-0b655546e67cc6796"
+    "latest.eu-west-1.hvm-ebs" = "ami-0b954e68de379e2dc"
   }
   description = "A map of release series to actual releases"
 }
